5d99808ee0ed5ee8a05d9886a5a22fb3f697adb7
vijay <vijay.com>
2021-07-08 19:50:28 +0530

    completed practical

--- app/navigation/home-navigator.tsx
+++ app/navigation/home-navigator.tsx
@@ -1,17 +1,14 @@
 import React, { } from 'react';
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import HomeScreen from '../screens/home/home-screen';
 import FavoriteScreen from '../screens/favorite/favorite-screen';
-import { SafeAreaView } from 'react-native-safe-area-context';
-const Tab = createMaterialTopTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
 export default function HomeNavigator() {
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Favorite" component={FavoriteScreen} />
-      </Tab.Navigator>
-    </SafeAreaView>
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Favorite" component={FavoriteScreen} />
+    </Tab.Navigator>
   );
 }
